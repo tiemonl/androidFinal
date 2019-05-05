60f2ca31b3300e2e879a40e8f88923a0c6d01db4
Liam <tiemon.liam@gmail.com>
2019-05-04 20:20:56 -0400

    removed retrofit and added background call

--- app/src/main/kotlin/com/example/androidfinal/MainActivity.kt
+++ app/src/main/kotlin/com/example/androidfinal/MainActivity.kt
@@ -2,11 +2,36 @@ package com.example.androidfinal
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import org.jetbrains.anko.doAsync
+import org.jetbrains.anko.uiThread
+import java.net.URL
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var button: Button
+    private lateinit var txtview: TextView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        button = findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            update()
+        }
+    }
+
+    private fun update() {
+        doAsync {
+            //Execute all the lon running tasks here
+
+            val apiResponse = URL("https://my-json-server.typicode.com/tiemonl/androidFinal/authors").readText()
+            uiThread {
+                //Update the UI thread here
+                txtview = findViewById(R.id.textview)
+                txtview.text = apiResponse
+            }
+        }
+
     }
 }
